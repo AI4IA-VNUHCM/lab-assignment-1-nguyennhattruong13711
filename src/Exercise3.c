6ad262f50a67a9bd2d06aa809bdd96524a4bce69
nguyennhattruong13711 <91869002+nguyennhattruong13711@users.noreply.github.com>
2021-10-04 21:11:08 +0700

    Update Ex 3

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -16,8 +16,8 @@ int snt(int x)
 {
 	if (x < 2)
 	return 0;
-	for (int i =2; i <= sqrt(x) i++)
-	if (x%i == o)
+	for (int i =2; i <= sqrt(x); i++)
+	if (x%i == 0)
 	return 0;
 	return 1;
 }
@@ -26,6 +26,6 @@ int main() {
 	for (int i = 1; i < testcase; i++)
 	if (snt(i) == 1)
 	printf("%d",i);
-}
-return o;
+return 0;
+
 }
